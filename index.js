6568eb6d50052c1a517a7ed0696677f7d688da49
BuGalter <wardoc78@gmail.com>
2022-01-06 10:38:39 +0300

    Added to index function generateNumber and her discription

--- index.js
+++ index.js
@@ -3,6 +3,23 @@
  * @module testtask-permutations/index
  */
 
+function generateNumber(n) {
+  /**
+   * The function returns an array that contains the start number.
+   * @param {number} n - Number of digits in a number.
+   * @return {Array<number>} number - The start number.
+   */
+  const repeatingDigit = 0;
+  let number = [];
+  for (let i = 0; i < n; i += 1) {
+    number[i] = repeatingDigit;
+  };
+  for(let j = 1; j < (n + 1); j += 1) {
+    number.push(j);
+  };
+  return number;
+};
+
 function permutations(n) {
   /**
    * The function returns a text file that contains all permutations without repetitions,
@@ -12,5 +29,8 @@ function permutations(n) {
    * all permutations.
    */
   let numberLinesFile = 0;
+  let number = generateNumber(n);
   return numberLinesFile;
 };
+
+console.log(permutations(7));
