d320d30caf13a0d1a89d0e4c41a379312b8c385e
BuGalter <wardoc78@gmail.com>
2022-01-06 10:25:10 +0300

    Added to index main function permutations and her discription

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
  * pemutations module.
  * @module testtask-permutations/index
  */
+
+function permutations(n) {
+  /**
+   * The function returns a text file that contains all permutations without repetitions,
+   * n identical and different numbers from 1 to n.
+   * @param {number} n - Number of digits in a number.
+   * @return {number} numberLinesFile - The number of lines in the file. And file out.txt with
+   * all permutations.
+   */
+  let numberLinesFile = 0;
+  return numberLinesFile;
+};
